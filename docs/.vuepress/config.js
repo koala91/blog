bd1da167f066d412cd67f294a24b0b4a6fe86d3d
xiewei <731390309@qq.com>
2021-07-19 00:49:05 +0800

    feat: logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,12 @@
 module.exports = {
   title: "My Blog",
-  description: "This is a blog."
-  // base: '/blog/' 
-}
+  description: "This is a blog.",
+  themeConfig: {
+    logo: "/assets/logo.jpg",
+  },
+  markdown: {
+    lineNumbers: true,
+  },
+  evergreen: true,
+  // base: '/blog/'
+};
