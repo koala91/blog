a11ab4e037431d5a58c2576192c8bb9a8f859f95
xiewei <731390309@qq.com>
2021-07-18 18:23:05 +0800

    fix: 分支

--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -15,6 +15,7 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
+git branch -M main
 
 # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:koala91/koala91.github.io.git main
