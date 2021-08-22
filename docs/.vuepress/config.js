434d8469f526824ba106a40719a2aad5466cb5ca
xiewei <731390309@qq.com>
2021-08-22 18:32:10 +0800

    feat: 新增React

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,10 +3,23 @@ module.exports = {
   description: "This is a blog.",
   themeConfig: {
     logo: "/assets/logo.jpg",
+    sidebar: [
+      {
+        title: "React",
+        collapsable: false,
+        sidebarDepth: 1,
+        children: [
+          "/React/JSX",
+          "/React/组件",
+          "/React/表单",
+          "/React/路由",
+          "/React/生命周期",
+        ],
+      },
+    ],
   },
   markdown: {
     lineNumbers: true,
   },
   evergreen: true,
-  // base: '/blog/'
 };
---
+++ docs/React/JSX.md
@@ -0,0 +1,7 @@
+# JSX
+
+在 React 中使用 JSX 语法描述用户界面，它是一种 JavaScript 语法扩展。
+
+在 React 代码执行之前，Babel 会将 JSX 语法转换为标准的 JavaScript API。
+
+JSX 语法就是一种语法糖，让开发人员使用更加舒服的代码构建用户界面。
